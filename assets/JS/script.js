899f30a8efb5205843e3ce5df16f292781973a68
Ei Sin <einge174@gmail.com>
2024-08-15 22:31:08 +0000

    Nav and Letbutton style added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -91,10 +91,5 @@
             sessionStorage.clear();
         }
 
-        fetch("https://ci-swapi.herokuapp.com/api/")
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("content").innerText = data;
-            })
-    
+        
         
