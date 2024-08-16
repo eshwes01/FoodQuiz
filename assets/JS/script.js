7ed2bd44494c117d113d07ce20a765e414a617e1
Ei Sin <einge174@gmail.com>
2024-08-16 22:16:09 +0000

    add code for Saved answer question and load answer and fixing the style for question layout

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,5 @@
         let currentQuestion = 1;
         const totalQuestions = 3;
-
         const quizForm = document.getElementById("quizForm");
 
         function hideForm(){
@@ -9,48 +8,67 @@
 
         // Quiz Display will be hidden when the page loaded
         window.onload = function() {
-           
             showQuestion(currentQuestion);
         };
 
-        // Display the first question
-        //document.getElementById(`question${currentQuestion}`).style.display = 'block';
 
+        /**
+         * Display the first question, starting the quiz 
+         */
         function showQuestion(questionNumber) {
              //Hide all questions
              for (let i = 1; i <= totalQuestions; i++) {
                 document.getElementById(`question${i}`).style.display = 'none';
             }
             //Show the current question
-            document.getElementById(`question${questionNumber}`).style.display = 'block';
-            
+            document.getElementById(`question${questionNumber}`).style.display = 'flex'; 
+             //document.getElementById(`question${currentQuestion}`).style.display = 'block'; 
         }
 
-
-        function loadAnswer() {
+        /**
+         *  Saving user answer of the current question and add that in session storage
+         */
+        function saveAnswer(){
             const form = document.forms['quizForm'];
-            const question = `q${currentQuestion}`;
-            const savedAnswer = sessionStorage.getItem(question);
-            if (savedAnswer) {
-                form[question].value = savedAnswer;
+            const currentQuestion = `q${currentQuestion}`;
+            const currentAnswer = form[currentQuestion].value;
+
+            if(currentAnswer){
+                sessionStorage.setItem(currentQuestion,currentAnswer);
+            }
+        }  
+
+        /**
+         *  Retrieving and read user answers from the session storage 
+         */
+        function loadAnswer(){
+            const form = document.forms['quizForm'];
+            const currentQuestion = `q${currentQuestion}`;
+            const savedAnswer = sessionStorage.getItem(currentQuestion);
+
+            if (savedAnswer){
+                form[currentQuestion].value = savedAnswer;
             }
         }
 
+        /**
+         * Submit Quiz and checking with user answer if match 
+         */
         function submitQuiz() {
             saveAnswer();
             
             // Store correct answers
             const correctAnswers = {
                 q1: 'b',  // taco
-                q2: 'c',  // spaghetti
+                q2: 'c',  // pot au feu
                 q3: 'a',  // Paella
-                q4: 'c',  //
-                q5: 'b',  // 
-                q6: 'c',  // 
-                q7: 'a',  // 
-                q8: 'c',  //
-                q9: 'a',  //
-                q10: 'b'
+                q4: 'd',  // Moussaka
+                q5: 'b',  // Goulash
+                q6: 'c',  // Kim chi
+                q7: 'c',  // Borscht
+                q8: 'b',  // Bao Bun
+                q9: 'a',  // Ceviche
+                q10: 'd'  // Green Curry
             };
 
             let score = 0;
